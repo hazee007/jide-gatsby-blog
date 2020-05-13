0b09d2a4e34f1b2ddb880c0ae191f39919a04b96
hazee007 <ajimoh2012@yahoo.com>
2020-05-13 18:49:33 +0400

    adding the word post

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>Azeez's thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlockLink to={node.fields.slug}>
